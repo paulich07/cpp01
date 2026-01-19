d0809f4e675e12ee179ec651ede93250d2aada4a
paulich07 <50242426+paulich07@users.noreply.github.com>
2026-01-19 17:44:00 +0100

    gurlll

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,25 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:00:40 by plichota          #+#    #+#             */
-/*   Updated: 2026/01/18 19:00:40 by plichota         ###   ########.fr       */
+/*   Updated: 2026/01/19 17:43:51 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanB.hpp"
+
+HumanB::HumanB()
+{
+}
+
+HumanB::HumanB(std::string name) : name(name)
+{
+}
+
+HumanB::~HumanB()
+{
+}
+
+void HumanB::attack()
+{
+    std::cout << name << " attacks with their " << weapon.getType() << std::endl;
+}
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -6,7 +6,20 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:00:42 by plichota          #+#    #+#             */
-/*   Updated: 2026/01/18 19:00:42 by plichota         ###   ########.fr       */
+/*   Updated: 2026/01/19 17:43:32 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+class HumanB
+{
+    private:
+        std::string name;
+        Weapon weapon;
+    public:
+        HumanB();
+        HumanB(std::string name);
+        ~HumanB();
+        void attack();
+};
